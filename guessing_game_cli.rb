19889c50cfc5156909c1194203399a6b69004017
Kiri Um <kirirotha@gmail.com>
2020-07-08 17:20:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def run_guessing_game
-  secret_number = rand(1..6)
+  secret_number = rand(5) + 1
   #guess = 0 
   #while guess != secret_number do
     puts "Guess the secret number (1-6)." 
