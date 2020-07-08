7091cbf5974cc50dc61da1372478202e0a8301cb
Kiri Um <kirirotha@gmail.com>
2020-07-08 17:11:19 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,9 +5,9 @@ def run_guessing_game
   while guess != secret_number do
     puts "Guess the secret number (1-6)." 
     guess = gets.chomp
-    if guess == secret_number
+    if guess.to_i == secret_number
       puts "You guessed the correct number!"
-    elsif guess != secret_number
+    elsif guess.to_i != secret_number
       puts "Sorry! The computer guessed #{secret_number}."
     elsif guess == "exit"
       puts "Goodbye!"
